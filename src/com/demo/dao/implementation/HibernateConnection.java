9b43483509bbd6d7becf0560e0a7847ed626c2da
nastse <38038084+nastse@users.noreply.github.com>
2018-05-29 16:58:39 +0200

    29-05-2018 Actualizacion 1
    Configurado HibernteConnection para AWS

--- src/com/demo/dao/implementation/HibernateConnection.java
+++ src/com/demo/dao/implementation/HibernateConnection.java
@@ -15,14 +15,20 @@ public class HibernateConnection {
 	
 	public static SessionFactory doHibernateConnection() {
 		
-		String host = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
-		String port = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+//		String host = System.getenv("RDS_INSTANCE_HOST");
+//		String port = System.getenv("RDS_INSTANCE_PORT");
+		
+		String host = "root.c9p7jdg748qx.us-east-2.rds.amazonaws.com";
+		String port = "3306";
+		String userName = "useruS5";
+		String password= "kQ0cfsXn";
+		
 		
 		Properties database = new Properties();
 		database.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
-		database.setProperty("hibernate.connection.username", "useruS5");
-		database.setProperty("hibernate.connection.password", "kQ0cfsXn");
-		database.setProperty("hibernate.connection.url", "jdbc:mysql://"+host+":"+port+"/root");
+		//database.setProperty("hibernate.connection.username", "useruS5");
+		//database.setProperty("hibernate.connection.password", "kQ0cfsXn");
+		database.setProperty("hibernate.connection.url", "jdbc:mysql://"+host+":"+port+"/innodb?user=" + userName + "&password=" + password);
 		database.setProperty("hibernate.connection.dialect", "org.hibernate.dialect.MySQL5Dialect");
 		
 		//AÑADO LAS CLASES PARA LA CONEXION CON LA BASE DE DATOS
